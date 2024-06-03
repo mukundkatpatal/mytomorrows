bb2d0c9c8d50d41b04893a04a782101cdf030a18
Mukund Katpatal <mukund.katpatal@aerdata.com>
2024-06-03 19:14:04 +0200

    feat(mytomorrows): Build basic client app to make calls to random clinical trials

--- apps/mytomorrows/src/app/app.component.ts
+++ apps/mytomorrows/src/app/app.component.ts
@@ -1,14 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ApiclientService } from '@myt/services';
+import { environment } from './app.config';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  providers: [ApiclientService, HttpClient]
 })
 export class AppComponent {
   title = 'mytomorrows';
+
+  /**
+   *
+   */
+  constructor(private readonly service: ApiclientService) {
+      
+    this.service.getRandomStudies(this.clinicalTrialApiUrl).subscribe(x => console.log(x));
+  }
+
+  private get clinicalTrialApiUrl(): string {
+    let clinicalTrialsUrl = `${environment.clinicalTrialBaseUrl}${environment.clinicalTrialUrlVersion}${environment.studyResource}`;
+    const columns = ['NCTId','BriefTitle','OverallStatus','StartDate','CompletionDate','StudyFirstSubmitDate'];
+    const filters = '&format=json&pageSize=1';
+    clinicalTrialsUrl = `${clinicalTrialsUrl}?fields=${columns.join('|')}${filters}`;
+    return clinicalTrialsUrl;
+  }
 }
--- apps/mytomorrows/src/app/app.config.ts
+++ apps/mytomorrows/src/app/app.config.ts
@@ -2,11 +2,20 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideHttpClient()
   ],
 };
+
+export const environment = {
+  production: true,
+  clinicalTrialBaseUrl: 'https://clinicaltrials.gov/api/',
+  clinicalTrialUrlVersion: 'v2/',
+  studyResource: 'studies'
+}
