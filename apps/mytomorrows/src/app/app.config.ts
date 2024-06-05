ebd575f2847491175a2d5420bb253a60190b3b00
Mukund Katpatal <mukund.katpatal@aerdata.com>
2024-06-05 07:55:33 +0200

    chore(mytomorrows): Remove SSR support

--- apps/mytomorrows/src/app/app.config.ts
+++ apps/mytomorrows/src/app/app.config.ts
@@ -1,13 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideHttpClient(withFetch()),
