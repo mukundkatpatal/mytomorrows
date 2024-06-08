483516ce75088f03eb73227a74c0461aa2f841f3
Mukund Katpatal <mukund.katpatal@aerdata.com>
2024-06-08 08:19:47 +0200

    fix(services): Make sure the distinct date ranges dont overlap

--- libs/services/src/lib/services/dates.utils.ts
+++ libs/services/src/lib/services/dates.utils.ts
@@ -12,25 +12,34 @@ export function formatDate(date: Date): string {
 export function generateDateRanges(startDate: Date = new Date(1999, 8, 17),
     endDate: Date = new Date(), maxRange = 10): string[][] {
 
-  const oneDay = 24 * 60 * 60 * 1000;
-  const totalDays = Math.floor(
-    (endDate.getTime() - startDate.getTime()) / oneDay
-  );
+      const oneDay = 24 * 60 * 60 * 1000;
+      const totalDays = Math.floor((endDate.getTime() - startDate.getTime()) / oneDay);
+    
+      const ranges: string[][] = [];
+      const chosenRanges: { start: number, end: number }[] = [];
+    
+      while (ranges.length < maxRange) {
+        const randomStartDay = getRandomInt(0, totalDays - 31);
+        const rangeStartDate = new Date(startDate.getTime() + randomStartDay * oneDay);
+        const rangeEndDate = new Date(rangeStartDate.getTime() + 30 * oneDay);
+    
+        if (!isOverlapping(rangeStartDate, rangeEndDate, chosenRanges, oneDay)) {
+          chosenRanges.push({ start: randomStartDay, end: randomStartDay + 30 });
+          ranges.push([formatDate(rangeStartDate), formatDate(rangeEndDate)]);
+        }
+      }
+    
+      return ranges;
+}
 
-  const ranges: string[][] = [];
-  const chosenStartDays = new Set<number>();
+function isOverlapping(rangeStart: Date, rangeEnd: Date, chosenRanges: { start: number, end: number }[], oneDay: number): boolean {
+  for (const range of chosenRanges) {
+    const existingStart = new Date(range.start * oneDay);
+    const existingEnd = new Date(range.end * oneDay);
 
-  while (ranges.length < maxRange) {
-    const randomStartDay = getRandomInt(0, totalDays - 31);
-    if (!chosenStartDays.has(randomStartDay)) {
-      chosenStartDays.add(randomStartDay);
-      const rangeStartDate = new Date(
-        startDate.getTime() + randomStartDay * oneDay
-      );
-      const rangeEndDate = new Date(rangeStartDate.getTime() + 30 * oneDay);
-      ranges.push([formatDate(rangeStartDate), formatDate(rangeEndDate)]);
+    if ((rangeStart <= existingEnd && rangeStart >= existingStart) || (rangeEnd <= existingEnd && rangeEnd >= existingStart)) {
+      return true;
     }
   }
-
-  return ranges;
+  return false;
 }
