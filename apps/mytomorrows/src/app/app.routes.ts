b9c5668edc219d1899c2bfcf7e8ce9627c877a9f
Mukund Katpatal <mukund.katpatal@aerdata.com>
2024-06-19 21:41:11 +0200

    build(mytomorrows): enable named chuncks

--- apps/mytomorrows/src/app/app.routes.ts
+++ apps/mytomorrows/src/app/app.routes.ts
@@ -1,36 +1,43 @@
 import { Route } from '@angular/router';
+/**
+ * Create named chunks for each route
+ */
 export const appRoutes: Route[] = [
+  {
+    path: 'studies',
+    loadComponent: () =>
+      import('./features/studies/studies-list.component').then(
+      (c) => c.StudiesListComponent
+      ),
+      title: 'Studies',
+      data: { chunkName: 'studies'}
+    },
     {
-        path: 'studies',
-        loadComponent: () =>
-          import('./features/studies/studies-list.component').then(
-            (c) => c.StudiesListComponent
-          ),
-          title: 'Studies'
-      },
-      {
-        path: 'favorites',
-        loadComponent: () =>
-          import('./features/favorites/favorites.component').then(
-            (c) => c.FavoritesComponent
-          ),
-          title: 'Studies'
-      },
-      {
-        path: 'home',
-        loadComponent: () =>
-          import('./features/home/home.component').then(
-            (c) => c.HomeComponent
-          ),
-          title: 'Home'
-      },
-      {
-        path: '',
-        pathMatch: 'full',
-        loadComponent: () =>
-          import('./features/home/home.component').then(
-            (c) => c.HomeComponent
-          ),
-          title: 'Home'
-      },
+    path: 'favorites',
+    loadComponent: () =>
+      import('./features/favorites/favorites.component').then(
+      (c) => c.FavoritesComponent
+      ),
+      title: 'Favorites',
+      data: { chunkName: 'favorites'}
+    },
+    {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home.component').then(
+      (c) => c.HomeComponent
+      ),
+      title: 'Home',
+      data: { chunkName: 'home'}
+    },
+    {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./features/home/home.component').then(
+      (c) => c.HomeComponent
+      ),
+      title: 'Home',
+      data: { chunkName: 'home'}
+    },
 ];
